0e352dc359a17ef29692bc906ae5146f9ea5ad7e
Sonali <sonali.atkar@kratinsolutions.com>
2022-01-24 02:11:48 -0800

    test added

---
+++ cypress/integration/test_spec.js
@@ -0,0 +1,9 @@
+describe('test case',()=>{
+    it('verify visit',()=>{
+         cy.visit('http://localhost:8010')
+    })
+    it('verify test',()=>{
+        cy.get('.display-4').should('be.visible')
+        cy.get('.display-4').should('have.text','Welcome')
+    })
+})
