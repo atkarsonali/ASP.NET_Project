d3d32655be177e9c9dd2de83b55a7b1c51aa346b
Sonali <sonali.atkar@kratinsolutions.com>
2022-02-01 03:56:08 -0800

    pageload added

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -1,6 +1,6 @@
 describe('test case',()=>{
     it('verify visit',()=>{
-         cy.visit('http://localhost:8010', { responseTimeout: 2000000 })
+         cy.visit('http://localhost:8010', { responseTimeout: 2000000 , timeout: 1000000 })
     })
     it('verify test',()=>{
         cy.get('.display-4').should('be.visible')
