7aa5c2417a3e21f6ee3eb53bf3105f05144fd3b7
Sonali <sonali.atkar@kratinsolutions.com>
2022-02-02 03:23:13 -0800

    visit digital

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -1,9 +1,11 @@
 describe('test case',()=>{
     it('verify visit',()=>{
-         cy.visit('http://localhost:8010', { responseTimeout: 2000000 , timeout: 1000000 })
+         cy.visit('http://localhost:8020', { responseTimeout: 2000000 , timeout: 1000000 })
+         cy.wait(20000)
     })
-    it('verify test',()=>{
+    /*it('verify test',()=>{
         cy.get('.display-4').should('be.visible')
         cy.get('.display-4').should('have.text','Welcome')
-    })
+    })*/
+    
 })
